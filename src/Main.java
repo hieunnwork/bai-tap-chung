529f99846ca208de8f65c386eec2bbbcb972e4d0
unknown <hieunn@gmail.com>
2023-05-07 13:39:26 +0700

    add function search information

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ManagementDocument managementDocument = new ManagementDocument();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Nhập thông tin nhà xuất bản cần tìm kiếm");
+        String keyWord = scanner.nextLine();
+        managementDocument.searchDocument(keyWord);
     }
 }
